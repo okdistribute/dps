0a9f76bcb21fd0bb0a77f649e4324a033fc54c2e
Karissa McKelvey <krmckelv@gmail.com>
2015-09-18 14:27:27 -0700

    add and update

--- bin/add.js
+++ bin/add.js
@@ -1,6 +1,4 @@
-var path = require('path')
-var download = require('../lib/util/download.js')
-var config = require('../lib/util/config.js')
+var addSource = require('../lib/add.js')
 var usage = require('../lib/util/usage.js')('add.txt')
 
 module.exports = {
@@ -21,29 +19,8 @@ module.exports = {
 
 function handleAdd (args) {
   var location = args._[0]
-  var id = args._[1] || normalize(args._[0])
-
-  var sources = config.read(args).sources
-
-  if (sources[id]) return console.error('Source exists.')
-
-  var source = {
-    path: name,
-    location: location,
-    format: args.format,
-    type: args.type
-  }
-
-  download(source, function (err) {
-    if (err) throw err
-    success()
+  addSource(location, args, function (err) {
+    if (err) return console.error(err.message)
+    console.error('Source added successfully.')
   })
-
-  function success () {
-    config.addSource(id, source, args)
-  }
-}
-
-function normalize (source) {
-  return source.replace('\/','_').replace(/[^a-z_+A-Z0-9]/ig, '')
 }
--- bin/update.js
+++ bin/update.js
@@ -1,6 +1,5 @@
-var config = require('../lib/util/config.js')
+var update = require('../lib/update.js')
 var usage = require('../lib/util/usage.js')('update.txt')
-var download = require('../lib/util/download.js')
 
 module.exports = {
   name: 'update',
@@ -10,18 +9,8 @@ module.exports = {
 
 function handleUpdate (args) {
   var name = args._[0]
-  var sourceList = config.read(args).sources
-
-  if (name) return updateOne(sourceList[name])
-
-  for (var name in sourceList) {
-    updateOne(sourceList[name])
-  }
-}
-
-function updateOne (source) {
-  download(source, function (err) {
-    if (err) throw err
-    console.log('Updated successfully.')
+  update(name, args, function (err) {
+    if (err) return console.error(err.message)
+    console.error('Update complete.')
   })
 }
