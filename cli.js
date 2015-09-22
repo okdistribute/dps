cc969dbc51f8826c994c3dd72d4cd2e847b067a3
Karissa McKelvey <krmckelv@gmail.com>
2015-09-22 00:34:09 -0700

    big things

--- cli.js
+++ cli.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 var args = require('minimist')(process.argv.slice(2))
 var fs = require('fs')
-var dps = require('./')
+var dps = require('./')(args.path)
 
 var cmd = args._[0]
 
@@ -10,7 +10,7 @@ if (cmd === 'add') {
   if (!location || args.help) return usage('dps add <location>')
   return dps.add(location, args, function (err, source) {
     if (err) abort(err)
-    console.log(source)
+    done(source)
   })
 }
 
@@ -19,15 +19,28 @@ if (cmd === 'update') {
   if (!location || args.help) return usage('dps update [location]')
   return dps.update(location, args, function (err, source) {
     if (err) abort(err)
-    conosle.log(source)
+    done(source)
   })
 }
 
+if (cmd === 'destroy') {
+  dps.destroy(function (err) {
+    if (err) abort(err)
+    done('goodbye')
+  })
+}
+
+function done (message) {
+  dps.save(function (err) {
+    if (err) abort(err)
+    console.log(message)
+  })
+}
 
 usage(fs.readFileSync('./usage/root.txt').toString())
 
 function abort (err) {
-  console.error(err)
+  console.trace(err)
   process.exit(1)
 }
 
--- index.js
+++ index.js
@@ -1,4 +1,63 @@
-module.exports = {
-  add: require('./lib/add.js'),
-  update: require('./lib/update.js')
+var path = require('path')
+var download = require('../lib/util/download.js')
+
+CONFIG_FILE = 'dps.json'
+
+module.exports = function (dir) {
+  if (!dir) dir = process.cwd()
+
+  var dps = {}
+  var configPath = path.join(dir, CONFIG_FILE)
+
+  dps.config = readConfig(configPath)
+
+  dps.add = function (location, args, cb) {
+    var this = self
+    if (getSource(location)) return cb(new Error('Source exists.'))
+
+    var source = {
+      path: normalize(location),
+      location: location,
+      type: args.type
+    }
+
+    download(source, function (err) {
+      if (err) return cb(err)
+      updateSource(source)
+      cb(null, source)
+    })
+  }
+
+  dps.save = function (cb) {
+    fs.writeFile(configPath, JSON.stringify(config, null, 2), cb)
+  }
+
+  dps.destroy = function () {
+    for (var source in config.sources) {
+      rimraf.sync(source.path)
+    }
+    rimraf.sync(configPath)
+  }
+
+  function getSource (location) {
+    return config.sources[location]
+  }
+
+  function updateSource (source) {
+    source.last_updated = new Date()
+    config.sources[source.location] = source
+  }
+
+
+  return dps
+}
+
+
+function readConfig (configPath) {
+  if (fs.existsSync(configPath)) return JSON.parse(fs.readFileSync(configPath))
+  else return { sources: {} }
+}
+
+module.exports = function normalize (source) {
+  return source.replace('\/','_').replace(/[^a-z_+A-Z0-9]/ig, '')
 }
